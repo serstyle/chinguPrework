39951da3ea2fb4057e8542e4257bbaa09f8a7c08
francois <desobry.f@gmail.com>
2019-05-19 20:25:01 +0200

    Updates

---
+++ precache-manifest.223bc00395eaafb776677d8cd6d82344.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f02ec994e18e66e536df8d78d36f4063",
+    "url": "/chinguPrework/index.html"
+  },
+  {
+    "revision": "173f86524089560b3163",
+    "url": "/chinguPrework/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "704fbe14000602403110",
+    "url": "/chinguPrework/static/js/2.ccbb2550.chunk.js"
+  },
+  {
+    "revision": "173f86524089560b3163",
+    "url": "/chinguPrework/static/js/main.6f5e10fb.chunk.js"
+  },
+  {
+    "revision": "60ad7114e64964bdbd00",
+    "url": "/chinguPrework/static/js/runtime~main.d1411589.js"
+  }
+]);
